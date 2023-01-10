f0b8b3cc02720ad5ed8efd0e8d560698a1b1f0b1
Lenno Sousa <lenno_sousa@hotmail.com>
2023-01-10 11:07:26 -0300

    SOLID-01 - Exemplo de violação do SIP e aplicando as suas boas práticas para deixar o código coeso.

---
+++ DesignPatternsConsoleApp-Udemy/DesignPatternsConsoleApp/Utils/Notify.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsConsoleApp.Utils
+{
+    internal class Notify
+    {
+
+        // Dessa forma, estamos respeitando o SIP, removendo as atividades
+        // que não fazem parte do escopo da classe cliente, neste exemplo.
+        public Notify(Client client)
+        {
+            Console.WriteLine("Notificando o cliente");
+        }
+    }
+}
